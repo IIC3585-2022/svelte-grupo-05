7a342b693e57bc3ac55138a2006db821e961b574
Elias <elias.sabja@uc.cl>
2022-06-05 12:19:18 -0400

    Code cleaned :)

--- MyAnimes/src/routes.js
+++ MyAnimes/src/routes.js
@@ -1,7 +1,6 @@
 import Register from './views/Register.svelte';
 import Login from './views/Login.svelte';
 import MyAnimes from './views/MyAnimes.svelte';
-import MainPage from './views/MainPage.svelte';
 import AnimeProfile from './views/AnimeProfile.svelte';
 import HomePage from './views/HomePage.svelte';
 
--- MyAnimes/src/store.js
+++ MyAnimes/src/store.js
@@ -1,10 +1,5 @@
-import { writable, derived, get } from 'svelte/store';
+import { writable } from 'svelte/store';
 
 export const token = writable('')
 
 export const user = writable('')
-
-export const myAnimes = writable([]);
-export const updateMyAnimes = (newAnime) => {
-    myAnimes.set([...get(myAnimes), newAnime]);
-}
